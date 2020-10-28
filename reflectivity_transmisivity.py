d4c57538fd991620c889eb9d98c22933fd75aff3
knaaar <34011307+aknapper@users.noreply.github.com>
2020-10-28 00:34:24 -0400

    refactor for function methods, grapghing, csv generation

--- reflectivity_transmisivity.py
+++ reflectivity_transmisivity.py
@@ -1,7 +1,8 @@
 from math import pi, degrees, asin, cos, sqrt, radians
+import matplotlib.pyplot as plt
+import csv
 
 def cosd(angle):
-
     return cos(radians(angle))
 
 permeability_free_space = 4e-7*pi
@@ -10,40 +11,86 @@ permittivity_medium_1 = 1
 permittivity_medium_2 = 8
 
 transmitted_angle = degrees(asin(1/sqrt(permittivity_medium_2)))
-incident_angle = list(range(0,90))
+incident_angle = list(range(0,91))
 
-perpendicular_reflectivity = pow(   \
-                                 ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(45))-     \
-                                 (sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(transmitted_angle)))/   \
-                                 ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(45))+   \
-                                 (sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(transmitted_angle)))
-                                 ,2)
+def perpendicular_reflectivity(x):
+    return pow(   \
+               ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(x))-     \
+               (sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(transmitted_angle)))/   \
+               ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(x))+   \
+               (sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(transmitted_angle)))
+              ,2)
 
-print('TE R: ',perpendicular_reflectivity)
+def perpendicular_transmissivity(x):
+    return pow(
+               ((2*(sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(x)))/     \
+               (((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(x)))+     \
+               ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(transmitted_angle))))),2)*  \
+           ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(transmitted_angle)))/     \
+           ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(x)))
 
-perpendicular_transmissivity = pow(
-                                   ((2*(sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(45)))/     \
-                                   (((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(45)))+     \
-                                   ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(transmitted_angle))))),2)*  \
-                               ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(transmitted_angle)))/     \
-                               ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(45)))
+def parallel_reflectivity(x):
+    return pow(   \
+               ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(transmitted_angle))-     \
+               (sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(x)))/   \
+               ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(transmitted_angle))+   \
+               (sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(x)))
+              ,2)
 
-print('TE T: ', perpendicular_transmissivity)
+def parallel_transmissivity(x):
+    return pow(
+               ((2*(sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(x)))/     \
+               (((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(transmitted_angle)))+     \
+               ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(x))))),2)*  \
+            ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(transmitted_angle)))/     \
+            ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(x)))
 
-parallel_reflectivity = pow(   \
-                                 ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(transmitted_angle))-     \
-                                 (sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(45)))/   \
-                                 ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(transmitted_angle))+   \
-                                 (sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(45)))
-                                 ,2)
+te_reflect_incident = []
+te_transmissivity_incident = []
+tm_reflectivity_incident = []
+tm_transmissivity_incident = []
+te_reflectivity = []
+te_transmissivity = []
+tm_reflectivity = []
+tm_transmissivity = []
+for angle in incident_angle:
+    te_reflectivity.append(perpendicular_reflectivity(angle))
+    te_reflect_incident.append([angle,perpendicular_reflectivity(angle)])
 
-print('TM R: ',parallel_reflectivity)
+    te_transmissivity.append(perpendicular_transmissivity(angle))
+    te_transmissivity_incident.append([angle,perpendicular_transmissivity(angle)])
 
-parallel_transmissivity = pow(
-                                   ((2*(sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(45)))/     \
-                                   (((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(transmitted_angle)))+     \
-                                   ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(45))))),2)*  \
-                               ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_1))*cosd(transmitted_angle)))/     \
-                               ((sqrt(permeability_free_space/(permittivity_free_space*permittivity_medium_2))*cosd(45)))
+    tm_reflectivity.append(parallel_reflectivity(angle))
+    tm_reflectivity_incident.append([angle, parallel_reflectivity(angle)])
 
-print('TM T: ', parallel_transmissivity)
+    tm_transmissivity.append(parallel_transmissivity(angle))
+    tm_transmissivity_incident.append([angle,parallel_transmissivity(angle)])
+
+#generate plot
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(incident_angle, te_reflectivity, label='TE Reflectivity')
+ax.plot(incident_angle, te_transmissivity, label='TE Transmissivity')
+ax.plot(incident_angle, tm_reflectivity, label='TM Reflectivity')
+ax.plot(incident_angle, tm_transmissivity, label='TM Transmissivity')
+
+ax.set_xlim([0,90])
+ax.set_ylim([0,1])
+
+plt.xlabel('Incident Angle (Degrees)')
+
+ax.set_title('Reflectivity and Transmissivity as a function of the Incident Angle')
+ax.legend()
+plt.show()
+
+# generate table
+file = open('table.csv', 'w', newline='\n')
+
+with file:
+    write = csv.writer(file)
+    #ugly formatting, still needs to write to multiple columns
+    write.writerows([angle] for angle in incident_angle)
+    write.writerows([value] for value in te_reflectivity)
+    write.writerows([value] for value in te_transmissivity)
+    write.writerows([value] for value in tm_reflectivity)
+    write.writerows([value] for value in tm_transmissivity)
